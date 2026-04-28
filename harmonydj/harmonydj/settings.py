d3371bd62549515f08181038ca8b321e69515e2c
Kkiprop <kt227kiprop@gmail.com>
2026-04-28 22:56:49 +0300

    Domain

--- harmonydj/harmonydj/settings.py
+++ harmonydj/harmonydj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m+0=+t(hh7!=2^#tn5nxm=uvt*ihk&ozfjex0$sj#%c#)kkhlw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://harmonynyali.com/", "harmonynyali.com", "www.harmonynyali.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
